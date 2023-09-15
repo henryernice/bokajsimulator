eeacea308639a1f763f77766a93b43586f62d90b
henryernice <103133437+henryernice@users.noreply.github.com>
2023-09-15 09:43:22 +0200

    Made the "MOAN SCORE SYSTEM"

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 const audio = new Audio("./cat-meow-14536.mp3");
 
 const moans = [new Audio("./moan1.mp3"),new Audio("./moan2.mp3")]
+
+var score = 0;
 // const buttons = document.querySelectorAll("button");
 var bokajimage = document.getElementById("bokajimg");
 
@@ -14,6 +16,9 @@ console.log("JS running");
 // });
 
 function moan(){
+    var scorecounter = document.getElementById("counter");
+    score += 1;
+    scorecounter.innerText = "Moan score: " + String(score);
     let num = Math.ceil(Math.random()+0.5);
     moans[num-1].play();
 }
